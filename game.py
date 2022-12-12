f7b23688bfdeb018dc37ba12fe9ed832b390812c
Lucas Lofaro <lucasmlofaro@gmail.com>
2022-12-11 23:54:38 -0800

    simplify shift logic

--- game.py
+++ game.py
@@ -18,44 +18,41 @@ class Board:
         return self.tiles[i][j]
 
     def __setitem__(self, key, value):
-        i, j = key
-        self.tiles[i][j] = value
+        if not self.is_in_bounds(key):
+            return
+        row, col = key
+        self.tiles[row][col] = value
 
     def __repr__(self):
         value = ""
         for row in self.tiles:
-            value += str(row) + '\n'
+            row_str = " | ".join(map(str, row)) + "\n"
+            value += row_str
+            value += "-"*len(row_str) + "\n"
         return value
 
     def is_in_bounds(self, index):
-        i, j = index
-        return 0 <= i < self.dimension and 0 <= j < self.dimension
+        row, col = index
+        is_in_row_bounds = 0 <= row < self.dimension
+        is_in_col_bounds = 0 <= col < self.dimension
+        return is_in_row_bounds and is_in_col_bounds
 
     def shift(self, direction):
-        i, j = direction.value
+        board = Board()
+        row_shift, col_shift = direction.value
         for row in range(self.dimension):
             for col in range(self.dimension):
-                new_row = row + i
-                new_col = col + j
-                index = row, col
-                new_index = new_row, new_col
-                print(index, '->', new_index)
-                if not self.is_in_bounds(new_index):
-                    continue
-                self[new_index] = self[index]
-                self[index] = 0
+                board[row + row_shift, col + col_shift] = self[row, col]
+        return board
 
 
 if __name__ == '__main__':
     board = Board()
+    print(board)
 
     board[1, 1] = 1
     print(board)
-    board.shift(Direction.UP)
-    print(board)
-    board.shift(Direction.DOWN)
-    print(board)
-    board.shift(Direction.LEFT)
-    print(board)
-    board.shift(Direction.RIGHT)
-    print(board)
+    print(board.shift(Direction.UP))
+    print(board.shift(Direction.DOWN))
+    print(board.shift(Direction.LEFT))
+    print(board.shift(Direction.RIGHT))
