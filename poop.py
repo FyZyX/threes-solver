34a75703be023ab4e7d2b5f7f87f58cb0baab667
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-01-23 23:36:52 -0800

    basic board mechanics working

--- poop.py
+++ poop.py
@@ -1,48 +1,68 @@
+import itertools
+import random
+
 # Initialize the game board as a 4x4 grid of zeroes
-board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+]
+
+
+def random_index(row=None, col=None):
+    row = random.randint(0, 3) if row is None else row
+    col = random.randint(0, 3) if col is None else col
+    return row, col
 
 
 # Function to add a new tile to the board
-def add_tile(row=None, col=None):
-    import random
-    # Choose a random location on the board that is currently empty
-    x = row or random.randint(0, 3)
-    y = col or random.randint(0, 3)
-    while board[x][y] != 0:
-        x = row or random.randint(0, 3)
-        y = col or random.randint(0, 3)
-    # Add a new tile with a value of either 1 or 2
-    board[x][y] = random.choice([1, 2])
-
-
 def rotate_index(row, col, direction):
-    if direction == "UP":
+    if direction.upper() == "UP":
         return row, col
-    elif direction == "DOWN":
+    elif direction.upper() == "DOWN":
         return 3 - row, 3 - col
-    elif direction == "LEFT":
+    elif direction.upper() == "LEFT":
         return 3 - col, row
-    elif direction == "RIGHT":
+    elif direction.upper() == "RIGHT":
         return col, 3 - row
     else:
         print("Invalid direction")
     return row, col
 
 
+def add_tile(row=None, col=None):
+    # Choose a random location on the board that is currently empty
+    x, y = random_index(row, col)
+    while board[x][y] != 0:
+        x, y = random_index(row, col)
+    # Add a new tile with a value of either 1 or 2
+    board[x][y] = random.choice([1, 2])
+
+
+def spawn(direction):
+    while True:
+        row, col = random_index(row=3)
+        x, y = rotate_index(row, col, direction)
+        if board[x][y] == 0:
+            break
+    add_tile(x, y)
+
+
 def slide(direction):
-    for row in range(1, 4):
-        for col in range(4):
-            r, c = rotate_index(row, col, direction)
-            source = board[r][c]
-            destination = board[r - 1][c]
-            if source == 0:
-                continue
-            destination_is_empty = destination == 0
-            destination_is_equal = destination == source
-            if destination_is_empty or destination_is_equal:
-                board[r - 1][c] = source + destination
-                board[r][c] = 0
-    add_tile(3)
+    for row, col in itertools.product(range(1, 4), range(4)):
+        source_row, source_col = rotate_index(row, col, direction)
+        destination_row, destination_col = rotate_index(row - 1, col, direction)
+        source = board[source_row][source_col]
+        destination = board[destination_row][destination_col]
+        if source == 0:
+            continue
+        destination_is_empty = destination == 0
+        destination_is_equal = destination == source
+        if destination_is_empty or destination_is_equal:
+            board[destination_row][destination_col] = source + destination
+            board[source_row][source_col] = 0
+    spawn(direction)
 
 
 def move(direction):
@@ -60,7 +80,7 @@ def play():
         # Get input from the user for the next move
         move_dir = input("Enter direction to move (up, down, left, right): ")
         # Move the tiles and add a new tile
-        move("UP")
+        move(move_dir)
 
 
 if __name__ == '__main__':
