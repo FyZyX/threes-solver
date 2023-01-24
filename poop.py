ead8f8dad9c94be77f75db087c0839068e5d9678
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-01-24 03:46:15 -0800

    use object-oriented model

--- poop.py
+++ poop.py
@@ -1,86 +1,89 @@
 import itertools
 import random
 
-# Initialize the game board as a 4x4 grid of zeroes
-board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-]
+
+class Tile:
+    def __init__(self, value=0):
+        self.value = value
 
 
-def random_index(row=None, col=None):
-    row = random.randint(0, 3) if row is None else row
-    col = random.randint(0, 3) if col is None else col
-    return row, col
+class Index:
+    def __init__(self, row=None, col=None):
+        self.row = random.randint(0, 3) if row is None else row
+        self.col = random.randint(0, 3) if col is None else col
+
+    def rotate(self, direction):
+        if direction.lower().startswith("u"):
+            return Index(self.row, self.col)
+        elif direction.lower().startswith("d"):
+            return Index(3 - self.row, 3 - self.col)
+        elif direction.lower().startswith("l"):
+            return Index(3 - self.col, self.row)
+        elif direction.lower().startswith("r"):
+            return Index(self.col, 3 - self.row)
+        else:
+            print("Invalid direction")
 
 
-# Function to add a new tile to the board
-def rotate_index(row, col, direction):
-    if direction.upper() == "UP":
-        return row, col
-    elif direction.upper() == "DOWN":
-        return 3 - row, 3 - col
-    elif direction.upper() == "LEFT":
-        return 3 - col, row
-    elif direction.upper() == "RIGHT":
-        return col, 3 - row
-    else:
-        print("Invalid direction")
-    return row, col
+class Board:
+    def __init__(self):
+        # Initialize the game board as a 4x4 grid of zeroes
+        self.tiles = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        # self.tiles = [[Tile() for _ in range(4)] for _ in range(4)]
 
+    def __getitem__(self, index: Index):
+        return self.tiles[index.row][index.col]
 
-def add_tile(row=None, col=None):
-    # Choose a random location on the board that is currently empty
-    x, y = random_index(row, col)
-    while board[x][y] != 0:
-        x, y = random_index(row, col)
-    # Add a new tile with a value of either 1 or 2
-    board[x][y] = random.choice([1, 2])
+    def __setitem__(self, index: Index, value):
+        self.tiles[index.row][index.col] = value
 
+    def spawn(self, direction):
+        while True:
+            index = Index(row=3).rotate(direction)
+            if self[index] == 0:
+                break
+        self[index] = random.choice([1, 2])
 
-def spawn(direction):
-    while True:
-        row, col = random_index(row=3)
-        x, y = rotate_index(row, col, direction)
-        if board[x][y] == 0:
-            break
-    add_tile(x, y)
+    def slide(self, direction):
+        for row, col in itertools.product(range(1, 4), range(4)):
+            source_index = Index(row, col).rotate(direction)
+            destination_index = Index(row - 1, col).rotate(direction)
+            source = self[source_index]
+            destination = self[destination_index]
+            if source == 0:
+                continue
+            destination_is_empty = destination == 0
+            destination_is_equal = destination == source
+            if destination_is_empty or destination_is_equal:
+                self[destination_index] = source + destination
+                self[source_index] = 0
 
+    def move(self, direction):
+        self.slide(direction)
+        self.spawn(direction)
 
-def slide(direction):
-    for row, col in itertools.product(range(1, 4), range(4)):
-        source_row, source_col = rotate_index(row, col, direction)
-        destination_row, destination_col = rotate_index(row - 1, col, direction)
-        source = board[source_row][source_col]
-        destination = board[destination_row][destination_col]
-        if source == 0:
-            continue
-        destination_is_empty = destination == 0
-        destination_is_equal = destination == source
-        if destination_is_empty or destination_is_equal:
-            board[destination_row][destination_col] = source + destination
-            board[source_row][source_col] = 0
-    spawn(direction)
-
-
-def move(direction):
-    slide(direction)
+    def show(self):
+        # Print the current game board
+        for row in self.tiles:
+            print(row)
 
 
 def play():
-    add_tile()
-    add_tile()
+    board = Board()
+    board[Index()] = random.choice([1, 2])
+    board[Index()] = random.choice([1, 2])
     # Game loop
     while True:
-        # Print the current game board
-        for row in board:
-            print(row)
+        board.show()
         # Get input from the user for the next move
         move_dir = input("Enter direction to move (up, down, left, right): ")
         # Move the tiles and add a new tile
-        move(move_dir)
+        board.move(move_dir)
 
 
 if __name__ == '__main__':
