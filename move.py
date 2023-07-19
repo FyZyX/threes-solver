e1c1eca24c388d463798b27d6051c499f8992ae5
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-07-19 00:27:11 -0700

    it actually kinda works

--- move.py
+++ move.py
@@ -3,45 +3,44 @@ class Move:
         self.board = board
 
     def move_up(self):
-        for _ in range(4):  # ensures that all tiles have opportunity to move
-            for r in range(1, 4):
-                for c in range(4):
-                    self.move_merge(r, c, r-1, c)
+        for r in range(1, 4):
+            for c in range(4):
+                self.move_merge(r, c, r - 1, c)
 
     def move_down(self):
-        for _ in range(4):
-            for r in range(2, -1, -1):
-                for c in range(4):
-                    self.move_merge(r, c, r+1, c)
+        for r in range(2, -1, -1):
+            for c in range(4):
+                self.move_merge(r, c, r + 1, c)
 
     def move_left(self):
-        for _ in range(4):
-            for c in range(1, 4):
-                for r in range(4):
-                    self.move_merge(r, c, r, c-1)
+        for c in range(1, 4):
+            for r in range(4):
+                self.move_merge(r, c, r, c - 1)
 
     def move_right(self):
-        for _ in range(4):
-            for c in range(2, -1, -1):
-                for r in range(4):
-                    self.move_merge(r, c, r, c+1)
+        for c in range(2, -1, -1):
+            for r in range(4):
+                self.move_merge(r, c, r, c + 1)
 
     def move_merge(self, r1, c1, r2, c2):
-        # Get the Tile objects at (r1, c1) and (r2, c2)
         tile1 = self.board.board[r1][c1]
         tile2 = self.board.board[r2][c2]
 
-        # If the tile at (r1, c1) is None or the tile at (r2, c2) is not None and their values do not match, return
-        if not tile1 or (tile2 and tile1.get_value() != tile2.get_value()):
+        # If the tile at (r1, c1) is None, return
+        if not tile1:
             return
 
+        # If the tile at (r2, c2) is None, move the tile at (r1, c1) to (r2, c2)
+        if not tile2:
+            self.board.board[r2][c2] = tile1
+            self.board.board[r1][c1] = None
         # If both tiles are not None and their values are equal, merge them
-        if tile1 and tile2:
+        elif tile1.get_value() == tile2.get_value():
             tile2.set_value(tile1.get_value() + tile2.get_value())
             self.board.board[r1][c1] = None
-        # If the tile at (r2, c2) is None, move the tile at (r1, c1) to (r2, c2)
-        elif not tile2:
-            self.board.board[r2][c2] = tile1
+        # If one tile is 1 and the other is 2, merge them to create 3
+        elif {tile1.get_value(), tile2.get_value()} == {1, 2}:
+            tile2.set_value(3)
             self.board.board[r1][c1] = None
 
     def execute_move(self, direction):
