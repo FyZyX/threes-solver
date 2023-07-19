0740bd8ba29606597bbfe43174ea99945ef3975e
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-07-19 01:22:01 -0700

    clean up

--- tile.py
+++ tile.py
@@ -7,13 +7,15 @@ class Tile:
 
     def __eq__(self, other):
         if isinstance(other, Tile):
-            return self.value == other.value  # or whatever attributes you have in Tile class
+            return self.value == other.value
 
     def get_value(self):
         return self.value
 
     def merge(self, other):
-        if other and ((self.value, other.get_value()) in [(1, 2), (2, 1)] or (self.value == other.get_value() and self.value >= 3)):
+        is_1_2 = (self.value, other.get_value()) in [(1, 2), (2, 1)]
+        is_3ish = (self.value == other.get_value() and self.value >= 3)
+        if other and (is_1_2 or is_3ish):
             self.value = 3 if self.value in [1, 2] else self.value * 2
             return True
         return False
